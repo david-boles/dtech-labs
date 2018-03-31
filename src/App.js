d8b7faf1ccea1367aff26e32cfdd7b81232252ce
David Boles <me@davidbol.es>
2018-03-30 20:43:47 -0700

    Set up MDC Web (using RMDC)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import '../node_modules/material-components-web/dist/material-components-web.min.css';
 import './App.css';
+import {
+  Toolbar,
+  ToolbarRow,
+  ToolbarSection,
+  ToolbarTitle,
+  ToolbarMenuIcon,
+  ToolbarIcon
+} from 'rmwc/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Toolbar>
+          <ToolbarRow>
+            <ToolbarSection alignStart>
+              <ToolbarMenuIcon use="menu"/>
+              <ToolbarTitle>Toolbar</ToolbarTitle>
+            </ToolbarSection>
+            <ToolbarSection alignEnd>
+              <ToolbarIcon use="save"/>
+              <ToolbarIcon use="print"/>
+            </ToolbarSection>
+          </ToolbarRow>
+        </Toolbar>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
