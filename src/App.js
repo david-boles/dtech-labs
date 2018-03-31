55d380630e2356eed0a0342e181013a5ded93760
David Boles <me@davidbol.es>
2018-03-30 21:05:36 -0700

    Testing some MDC

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import {
   ToolbarMenuIcon,
   ToolbarIcon
 } from 'rmwc/Toolbar';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryText,
+  ListItemGraphic,
+  ListItemMeta
+} from 'rmwc/List';
+import { Grid, GridCell } from 'rmwc/Grid';
 
 class App extends Component {
   render() {
@@ -26,6 +35,22 @@ class App extends Component {
             </ToolbarSection>
           </ToolbarRow>
         </Toolbar>
+
+        <Grid>
+          <GridCell desktop="2" tablet="1" phone="0"></GridCell>
+          <GridCell desktop="8" tablet="6" phone="4">Some content</GridCell>
+          <GridCell desktop="2" tablet="1" phone="0"></GridCell>
+        </Grid>
+
+        <List twoLine>
+          <ListItem>
+            <ListItemText>
+              Two-line item
+              <ListItemSecondaryText>Secondary text</ListItemSecondaryText>
+            </ListItemText>
+          </ListItem>
+        </List>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
